909e68250950564f5fd90972d5912b6c9f3b6262
labtestera1 <158797306+labtesterera1@users.noreply.github.com>
2026-06-22 09:54:49 +0530

    done
    done

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  * ----------------------------------------------------------------
  */
 
-const CACHE_VERSION = "2.9.2";
+const CACHE_VERSION = "2.9.3";
 const CACHE_NAME = `architectsmartcraft-v${CACHE_VERSION}`;
 
 const APP_SHELL_FILES = [
