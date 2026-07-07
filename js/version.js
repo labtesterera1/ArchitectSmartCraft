18375869e5e7e5a1103090e29ef360a46acc6d0e
labtestera1 <158797306+labtesterera1@users.noreply.github.com>
2026-07-07 10:35:36 +0530

    done
    done

--- js/version.js
+++ js/version.js
@@ -13,6 +13,6 @@
  * ----------------------------------------------------------------
  */
 
-export const APP_VERSION = "3.3.0";
+export const APP_VERSION = "3.4.0";
 export const APP_NAME = "ArchitectSmartCraft";
 export const APP_SHORT_NAME = "ASC";
